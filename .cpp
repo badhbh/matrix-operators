5912343eb3f73cf4fcc9759f8dc3bb3956022231
badhbh <37417520+badhbh@users.noreply.github.com>
2018-05-30 12:30:30 +0300

    Update .cpp

--- .cpp
+++ .cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <fstream>
 
@@ -13,25 +12,31 @@ public:
 		col = 0;
 		str = 0;
 		mot = NULL;
+		cout << "ini";
 	}
-	matrix(int m, int n) {
-		m = str;
-		n = col;
-		mot = NULL;
+	matrix(const int m, const int n) : str(m), col(n)
+	{
 		if (m > 0 && n > 0) {
-			mot = new int *[m];
-			for (int i = 0; i < m; ++i) {
-				mot[i] = new int[m];
-				for (int j = 0; j < n; ++j) {
+		
+		//mot = NULL;
+		
+			mot = new int *[str];
+			for (int i = 0; i < str; ++i) 
+			{
+				mot[i] = new int[col];
+				for (int j = 0; j < col; ++j) 
+				{
 					mot[i][j] = 0;
+					cout << "zn";
 				}
 			}
-			
+
 		}
 		else {
 			col = 0;
 			str = 0;
 			mot = NULL;
+			cout << "osh";
 		}
 
 	}
@@ -39,6 +44,7 @@ public:
 		for (int i = 0; i < str; ++i)
 			delete mot[i];
 		delete[] mot;
+		cout << "ds";
 
 	}
 	matrix(matrix const &klon)
@@ -48,23 +54,24 @@ public:
 		mot = new int *[str];
 		for (int i = 0; i < str; ++i)
 		{
-			mot[i] = new int[str];
-				for (int j = 0; j < col; ++j)
-				{
-					mot[i][j] = klon.mot[i][j];
-				}
+			mot[i] = new int[col];
+			for (int j = 0; j < col; ++j)
+			{
+				mot[i][j] = klon.mot[i][j];
+				cout << "cop";
+			}
 		}
 
 	}
-	
+
 	int & operator ()(const int i, const int j)
 	{
-			return mot[i][j];
+		return mot[i][j];
 	}
 	matrix operator * (const int k)
 	{
 		matrix creature(str, col);
-		for(int i = 0; i < str; ++i)
+		for (int i = 0; i < str; ++i)
 			for (int j = 0; j < col; ++j)
 			{
 				creature.mot[i][j] = this->mot[i][j] * k;
@@ -127,7 +134,7 @@ public:
 		}
 		return prod;
 	}
-	matrix dot (matrix const &klon)
+	matrix dot(matrix const &klon)
 	{
 		matrix loli(this->col, klon.str);
 		if (this->col == klon.str)
@@ -145,7 +152,7 @@ public:
 	}
 	matrix operator +=(const int yk)
 	{
-		
+
 		for (int i = 0; i < str; ++i)
 		{
 			for (int j = 0; j < col; ++j)
@@ -157,8 +164,8 @@ public:
 	}
 	matrix operator +=(matrix const &klon)
 	{
-		if(this->str == klon.str && this->col == klon.col)
-		{ 
+		if (this->str == klon.str && this->col == klon.col)
+		{
 			for (int i = 0; i < str; ++i)
 			{
 				for (int j = 0; j < col; ++j)
@@ -223,7 +230,7 @@ public:
 	void trans()
 	{
 		int per;
-		if (this->col > 0 && this->str > 0 )
+		if (this->col > 0 && this->str > 0)
 		{
 			for (int i = 0; i < this->str; ++i)
 			{
@@ -236,24 +243,53 @@ public:
 		}
 		else { cerr << "0"; }
 	}
-	friend ostream & operator <<(const matrix &klon, ostream &out)
+	friend ostream & operator <<(ostream &out, const matrix &klon)
 	{
 		for (int i = 0; i < klon.str; ++i)
 		{
-			for (int j = 0; j < klon.col; ++j)
-				out << klon.mot[i][j];
+			for (int j = 0; j < klon.col; ++j) 
+			{
+				out << klon.mot[i][j] << " ";
+			}
+				out << endl;
+			
 		}
+		return out;
 	}
-	friend istream & operator >>(const matrix &klon, istream &in)
+	friend istream & operator >>(istream &in, const matrix &klon)
 	{
 		for (int i = 0; i < klon.str; ++i)
 		{
 			for (int j = 0; j < klon.col; ++j)
 				in >> klon.mot[i][j];
 		}
+		return in;
+	}
+	int get_matr(int a, int b)
+	{
+		return mot[a][b];
+	}
+	int get_col()
+	{
+		return col;
+	}
+	int get_str()
+	{
+		return str;
+	}
+	void get_t(int a, int b, int c)
+	{
+		mot[a][b] = c;
 	}
 };
 int main()
 {
+	
+	ofstream out("out.txt");	
+	ifstream in("in.txt");
+	matrix a(4, 4), b(7, 8), c;
+	in >> a >> b;
+	out << a << b;
+	out << (a + b);
 	return 0;
 }
